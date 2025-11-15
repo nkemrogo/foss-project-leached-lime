338a6474f00ede1c2f76f6592806c7dfdb07cd1c
Ogungbade Shalom <badeshalom@gmail.com>
2025-11-15 17:23:21 +0100

    fix:minor changes

--- frontend/hotel-compliant-management-system/src/main.jsx
+++ frontend/hotel-compliant-management-system/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom';  // Import here
+import { BrowserRouter as Router } from 'react-router-dom';  // Import here!!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
